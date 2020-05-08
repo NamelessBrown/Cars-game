8dcd58abee2da7bcd02f74ac1238d46d17cc8646
Nameless <tyrebrown873@gmail.com>
2020-05-08 19:17:18 -0400

    change sf::rendertarget to sf::renderwindow

--- Cars/GameState.h
+++ Cars/GameState.h
@@ -5,7 +5,7 @@
 class GameState
 {
 public:
-    virtual void Render(sf::RenderTarget& target) = 0;
+    virtual void Render(sf::RenderWindow& window) = 0;
     virtual void Update(sf::RenderWindow& window, const float dt) = 0;
     virtual void HandleInput(sf::RenderWindow& window, sf::Event& events) = 0;
 };
