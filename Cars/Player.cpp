d78418b0fd1f2ea295d4a0214582cc6e3e51e4bc
Nameless <tyrebrown873@gmail.com>
2020-05-11 14:38:11 -0400

    Scaled the picture down, and moved the position

--- Cars/Player.cpp
+++ Cars/Player.cpp
@@ -4,6 +4,8 @@ Player::Player(sf::Texture& texture, sf::IntRect& textureRect)
 {
 	m_sprite.setTexture(texture);
 	m_sprite.setTextureRect(textureRect);
+	m_sprite.setScale(m_sprite.getScale() / 2.f);
+	m_sprite.setPosition(480.f, 450.f);
 }
 
 void Player::Update(float dt)
