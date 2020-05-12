32601ab82e9f484a8d6764bb23dddf20605b5ca2
Nameless <tyrebrown873@gmail.com>
2020-05-12 14:20:47 -0400

    Playstate can take the user to the gameover state

--- Cars/PlayState.cpp
+++ Cars/PlayState.cpp
@@ -1,9 +1,10 @@
 #include "PlayState.h"
 #include <iostream>
+#include "GameOverState.h"
 
-PlayState::PlayState(ResourceHolder& resourceManger, sf::IntRect& textureRect)
+PlayState::PlayState(ResourceHolder& resourceManger, sf::IntRect& textureRect, GameStateManager& gameStateManager)
 	:m_resourceManger(resourceManger), m_player(m_resourceManger.getTexture("Textures/cars.jpg"), textureRect), 
-	m_backgroundScroll(.5f), m_backgroundScrollSpeed(.5f)
+	m_backgroundScroll(.5f), m_backgroundScrollSpeed(.5f), m_gameStateManager(gameStateManager)
 {
 	m_background.setTexture(m_resourceManger.getTexture("Textures/road.jpg"));
 	m_background.setPosition(300.f, 0.f);
@@ -42,8 +43,15 @@ void PlayState::MoveBackground(const float dt)
 
 void PlayState::Update(sf::RenderWindow& window, const float dt)
 {
-	m_player.Update(dt);
-	MoveBackground(dt);
+	if (!m_player.isDead())
+	{
+		m_player.Update(dt);
+		MoveBackground(dt);
+	}
+	else
+	{
+		m_gameStateManager.PushState(new GameOverState(m_resourceManger, m_gameStateManager));
+	}
 }
 
 void PlayState::Render(sf::RenderWindow& window)
--- Cars/PlayState.h
+++ Cars/PlayState.h
@@ -3,11 +3,13 @@
 #include "GameState.h"
 #include "ResourceHolder.h"
 #include "Player.h"
+#include "Button.h"
+#include "GameStateManager.h"
 
 class PlayState : public GameState
 {
 public:
-    PlayState(ResourceHolder& resourceManger, sf::IntRect& textureRect);
+    PlayState(ResourceHolder& resourceManger, sf::IntRect& textureRect, GameStateManager& gameStateManager);
     PlayState(const PlayState&) = delete;
     PlayState& operator=(const PlayState&) = delete;
     ~PlayState();
@@ -19,8 +21,10 @@ public:
     void MoveBackground(const float dt);
 private:
     ResourceHolder& m_resourceManger;
+    GameStateManager& m_gameStateManager;
     sf::Sprite m_background;
 
+    Button m_upgradeButton;
     Player m_player;
 
     double m_backgroundScroll;
