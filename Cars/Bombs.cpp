11b1946eac0f666b8b48a74d3707c9f98e72be2c
Nameless <tyrebrown873@gmail.com>
2020-05-12 15:36:08 -0400

    Random damage chance from 1 to 10

--- Cars/Bombs.cpp
+++ Cars/Bombs.cpp
@@ -9,8 +9,10 @@ Bombs::Bombs(sf::Texture& texture)
 	std::mt19937 rng(rd());
 	std::uniform_real_distribution<float> distribution(300.f, 500.f);
 	std::uniform_real_distribution<float> speedDistribution(0.5f, 5.5f);
+	std::uniform_int_distribution<int> damageDistribution(1, 10);
 
 	m_speed = speedDistribution(rng);
+	m_damage = damageDistribution(rng);
 
 	m_sprite.setPosition(distribution(rng), 25.f);
 	m_sprite.setScale(m_sprite.getScale() / 4.f);
--- Cars/Bombs.h
+++ Cars/Bombs.h
@@ -8,6 +8,7 @@ public:
 	Bombs(sf::Texture& texture);
 	~Bombs();
 
+	const int& GetDamage() const { return m_damage; }
 	const sf::Sprite& GetSprite() const { return m_sprite; }
 	void Update(float dt);
 	void Render(sf::RenderWindow& window);
@@ -15,5 +16,6 @@ private:
 	sf::Sprite m_sprite;
 
 	float m_speed;
+	int m_damage;
 };
 
