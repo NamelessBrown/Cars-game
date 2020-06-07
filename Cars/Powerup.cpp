b709d723174e564627297db8eac27111c46b6fe1
Nameless <tyrebrown873@gmail.com>
2020-06-07 18:24:09 -0400

    Powerup.cpp

--- Cars/Powerup.cpp
+++ Cars/Powerup.cpp
@@ -9,7 +9,7 @@ Powerup::Powerup(sf::Texture& texture)
 	std::random_device rd;
 	std::mt19937 rng(rd());
 	std::uniform_real_distribution<float> distribution(300.f, 500.f);
-	std::uniform_real_distribution<float> speedDistribution(1.5f, 5.5f);
+	std::uniform_real_distribution<float> speedDistribution(.5f, 1.f);
 
 	m_sprite.setPosition(distribution(rng), 25.f);
 	m_sprite.setScale(m_sprite.getScale() / 4.f);
